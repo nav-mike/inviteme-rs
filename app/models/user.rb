1cbe72135f8534b01bdd5fc775a1b5ce35879b45
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-01-19 09:17:51 +0200

    config rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify strict: true
   acts_as_authentic { _1.crypto_provider = ::Authlogic::CryptoProviders::SCrypt }
 
   validates :email, format: { with: /@/, message: 'should look like an email address.' },
@@ -7,4 +8,10 @@ class User < ApplicationRecord
   validates :password, confirmation: { if: :require_password? },
                        length: { minimum: 8, if: :require_password? }
   validates :password_confirmation, length: { minimum: 8, if: :require_password? }
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:user)
+  end
 end
