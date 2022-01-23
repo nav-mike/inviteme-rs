85a769161f38809f7f00858eb3d8cb6f2310deec
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-01-23 12:56:39 +0200

    add edit campaign form

--- app/controllers/panel/campaigns_controller.rb
+++ app/controllers/panel/campaigns_controller.rb
@@ -1,11 +1,13 @@
 module Panel
   class CampaignsController < Panel::ApplicationController
-    before_action :set_campaign, only: %i[destroy]
+    before_action :set_campaign, only: %i[destroy edit]
 
     def index
       @new_campaign = Campaign.new(amount: 0.0)
       @campaigns = Campaign.where(owner: current_user).order(created_at: :asc).page(params[:page])
     end
+
+    def edit(); end
 
     def create
       @campaign = Campaign.new(campaign_params.merge(owner: current_user))
