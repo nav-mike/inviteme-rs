2823d1fdc679b6e58511b9278f502d09b24369f3
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-01-26 11:08:42 +0200

    fix menu selection

--- app/helpers/panel/pages_helper.rb
+++ app/helpers/panel/pages_helper.rb
@@ -1,2 +1,13 @@
 module Panel::PagesHelper
+  def overview?(params)
+    params[:controller] == 'panel/pages' && params[:action] == 'index'
+  end
+
+  def campaigns?(params)
+    params[:controller] == 'panel/campaigns'
+  end
+
+  def active_navbar_link_classes(flag)
+    flag ? ' bg-emerald-700 text-white border-l-2' : ' hover:bg-slate-900 hover:text-slate-300'
+  end
 end
