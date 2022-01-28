1456d7aef37fccada434b5b159a7298dce0ad7d9
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-01-28 16:12:48 +0200

    fix by prettier

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,34 +1,34 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ['menuDiv']
+  static targets = ["menuDiv"];
 
   isShow = false;
 
   connect() {
-    document.addEventListener('click', (event) => {
-      if (event.target.id !== 'menuImg' && event.target.id !== 'menuLink') {
-        this.isShow = false
-        this.toggleMenuDiv()
+    document.addEventListener("click", (event) => {
+      if (event.target.id !== "menuImg" && event.target.id !== "menuLink") {
+        this.isShow = false;
+        this.toggleMenuDiv();
       }
-    })
+    });
   }
 
   disconnect() {
-    document.removeEventListener('click', () => {
-      console.log(`click outside the menu`)
-    })
+    document.removeEventListener("click", () => {
+      console.log(`click outside the menu`);
+    });
   }
 
   toggleMenuDiv() {
-    this.isShow ? this.menuDivTarget.classList.remove('hidden') : this.menuDivTarget.classList.add('hidden')
+    this.isShow ? this.menuDivTarget.classList.remove("hidden") : this.menuDivTarget.classList.add("hidden");
   }
 
   toggle(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     this.isShow = !this.isShow;
 
-    this.toggleMenuDiv()
+    this.toggleMenuDiv();
   }
 }
