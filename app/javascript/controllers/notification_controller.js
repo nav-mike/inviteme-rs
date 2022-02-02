afb2d04c278ed6e100b847d7f0195de802c71ba5
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-02-02 16:43:35 +0200

    add delete request for notifications

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -1,12 +1,27 @@
 import { Controller } from "@hotwired/stimulus";
+import { destroy } from "@rails/request.js";
 
 export default class extends Controller {
   static targets = ["notification"];
 
-  close() {
+  close(event) {
+    event.preventDefault();
+    this.hide(async () => {
+      await this.destroy(this.notificationTarget.id.match(/\d+/)[0]);
+    });
+  }
+
+  hide(callback) {
     this.notificationTarget.classList.add("disappear");
     setTimeout(() => {
       this.notificationTarget.classList.add("hidden");
+      if (typeof callback === "function") callback();
     }, 400);
+  }
+
+  async destroy(id) {
+    await destroy(`/notifications/${id}.json`, {
+      contentType: "application/json",
+    });
   }
 }
