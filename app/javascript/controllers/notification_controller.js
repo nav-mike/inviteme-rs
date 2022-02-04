5cb599646e4b026417f91c14743502a9a636649e
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-02-04 11:18:02 +0200

    add auto destroy notification

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -2,20 +2,32 @@ import { Controller } from "@hotwired/stimulus";
 import { destroy } from "@rails/request.js";
 
 export default class extends Controller {
-  static targets = ["notification"];
+  static targets = ["notification", "progressbar"];
+
+  connect() {
+    const progressbar = this.progressbarTarget;
+    let width = 1000;
+    const id = setInterval(() => {
+      if (width <= 0) {
+        clearInterval(id);
+        this.hide();
+      } else {
+        width--;
+        progressbar.style.width = width / 10 + "%";
+      }
+    }, 10);
+  }
 
   close(event) {
     event.preventDefault();
-    this.hide(async () => {
-      await this.destroy(this.notificationTarget.id.match(/\d+/)[0]);
-    });
+    this.hide();
   }
 
-  hide(callback) {
+  hide() {
     this.notificationTarget.classList.add("disappear");
-    setTimeout(() => {
+    setTimeout(async () => {
       this.notificationTarget.classList.add("hidden");
-      if (typeof callback === "function") callback();
+      await this.destroy(this.notificationTarget.id.match(/\d+/)[0]);
     }, 400);
   }
 
