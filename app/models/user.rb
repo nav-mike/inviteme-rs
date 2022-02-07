ed839e5523c6ed8c1bf9a45271c0553e4cc9f83e
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-02-07 12:47:28 +0200

    auto update user data in the header

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,12 @@ class User < ApplicationRecord
 
   after_create :assign_default_role
 
+  after_save_commit if: :email_or_names_changed? do
+    broadcast_replace_to ActionView::RecordIdentifier.dom_id(self, 'header_user_full_name'),
+                         partial: 'panel/users/full_name',
+                         locals: { user: UserDecorator.decorate(self) }
+  end
+
   has_many :campaigns, dependent: :destroy
 
   def assign_default_role
@@ -22,5 +28,9 @@ class User < ApplicationRecord
 
   def require_password?
     new_record? || !password.nil? || !password_confirmation.nil?
+  end
+
+  def email_or_names_changed?
+    email_previously_changed? || first_name_previously_changed? || last_name_previously_changed?
   end
 end
