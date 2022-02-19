4027fd1a973a63c9ca5361a2c6addd9e1efabb5d
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-02-19 11:51:36 +0200

    add new comment button

--- app/javascript/controllers/comment_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  requestSubmit(event) {
-    console.log("requestSubmit");
-    event.preventDefault();
-  }
-}
