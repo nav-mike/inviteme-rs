d3b2fde9cd29f5868f7424ebd398df8437250bf4
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-03-14 14:36:04 +0200

    add edit team components

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,6 +4,11 @@ class UserDecorator < ApplicationDecorator
     result.strip.length.positive? ? result : email
   end
 
+  def full_name_with_email
+    result = "#{first_name} #{last_name}"
+    result.strip.length.positive? ? "#{result} (#{email})" : email
+  end
+
   def team_name
     current_team.name
   end
